5cc13117ca2a85015026b554bafa8630adb4c1e6
Kamila <kamillashirinova9r1@gmail.com>
2025-07-12 16:39:34 +0200

    basic UI

--- Battleships/Assets/UISquareScaler.cs
+++ Battleships/Assets/UISquareScaler.cs
@@ -1,22 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UISquareScaler : MonoBehaviour
 {
     [Header("Setup in Prefab")]
-    public RectTransform rectTransform;
+    public CanvasScaler canvasScaler;
+    public RectTransform canvasTransform;
+    public RectTransform viewportRectTransform;
+
+    public RectTransform leftRectTransform;
+    public RectTransform rightRectTransform;
+
 
     void Awake()
     {
-        Vector2 size = rectTransform.sizeDelta;
-        size.x = Screen.height;
-        rectTransform.sizeDelta = size;
+        Init();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Init()
     {
-        
+        float heightInPPU = canvasTransform.rect.height;
+        float widthInPPU = canvasTransform.rect.width;
+
+        viewportRectTransform.sizeDelta = new Vector2(heightInPPU, heightInPPU);
+
+        float halfRemainindWidth = (float)(widthInPPU - heightInPPU) / 2.0f;
+
+        leftRectTransform.sizeDelta = new Vector2(halfRemainindWidth, heightInPPU);
+        rightRectTransform.sizeDelta = new Vector2(halfRemainindWidth, heightInPPU);
     }
 }
